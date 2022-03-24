6fe7cb0b06b26552265afbeded2e68c350c8d3da
EVE JOYCE <103681990@student.swin.edu.au>
2022-03-24 16:37:47 +1100

    completed GET methods

--- DipGitApi/Controllers/ProductsController.cs
+++ DipGitApi/Controllers/ProductsController.cs
@@ -6,13 +6,17 @@ using RestSharp;
 using Microsoft.Extensions.Configuration;
 using DipGitApiLib;
 using System.Text.Json;
+using System.Net;
+using System.Linq;
 
 namespace DipGitApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
+
     {
+        static Products products = new Products();
         private readonly IConfiguration _config;
         private RestClient _client;
         private string _accessKey;
@@ -54,8 +58,22 @@ namespace DipGitApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll() {
             // return all item as a Products object
-            return BadRequest();
+            var request = new RestRequest();
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", _accessKey);
+            request.AddHeader("content-type", "application/json");
+            var response = await _client.GetAsync(request);
+
+            // TODO set condition
+            if(response.Content.Contains("_id")) {
+                return Ok(response.Content);
+                // TODO cast as products object
+            }
+
+            return NotFound();
         }
+
+        /*
 
         /// <summary>
         /// Add a new Product
@@ -64,8 +82,37 @@ namespace DipGitApi.Controllers
         /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Add(Product newProduct) {
-            return BadRequest();
+            // var testNewProduct = new Product();
+            // testNewProduct.Name = "Big Donut";
+            // testNewProduct.Price = 10F;
+            // testNewProduct.Qty = 10;
+
+            var newProductAltered = JsonSerializer.Serialize(newProduct);
+
+            var request = new RestRequest();
+            request.Method = Method.Post;
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", _accessKey);
+            // request.AddJsonBody(newProductAltered);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", newProductAltered, ParameterType.RequestBody);
+            Console.WriteLine(request.Parameters.Where(p => p.Type == ParameterType.RequestBody).FirstOrDefault());
+            var paramsList = request.Parameters.Where(p => p.Type == ParameterType.HttpHeader).ToArray();
+            for(var i =0; i<paramsList.Length;i++){
+                Console.WriteLine(paramsList[i].ToString());
+            }
+            var response = await _client.ExecuteAsync(request);
+
+            if(response.IsSuccessful){
+            return Ok(response.Content);
+            // if (response.StatusCode == HttpStatusCode.OK){
+            // }
+            } else{
+                return Problem(response.Content);
+            }
+
         }
+
 
         /// <summary>
         /// Deletes a product based on id
@@ -74,8 +121,20 @@ namespace DipGitApi.Controllers
         /// <returns></returns>
         [HttpDelete]
         public async Task<IActionResult> Delete(string id) {
-            return BadRequest();
+            var request = new RestRequest("", Method.Delete);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", _accessKey);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", $"{{\"id\":\"{id}\"}}", ParameterType.RequestBody);
+            var response = await _client.DeleteAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK){
+                return Ok(response.Content);
+            }
+
+            return NotFound();
         }
+
 
         /// <summary>
         /// Returns the total qty of item from all products
@@ -84,8 +143,14 @@ namespace DipGitApi.Controllers
         [HttpGet("GetTotalQty")]
         public async Task<IActionResult> GetTotalQty() {
             // Read all products and create a Products object.  Use the products object to determine the total qty
+            
+            // TODO set condition does this work
+            // products = GetAll();
+
+            // return products.GetTotalQtyProducts();
             return BadRequest();
         }
+
 
         /// <summary>
         /// Returns the total value of all item prices summed.
@@ -94,7 +159,12 @@ namespace DipGitApi.Controllers
         [HttpGet("GetTotalValue")]
         public async Task<IActionResult> GetTotalValue() {
             // Read all products and create a Products object.  Use the products object to determine the total value
+            // products = GetAll();
+
+            // return products.GetTotalValueProducts();
+
             return BadRequest();
         }
+        */
     }
 }
